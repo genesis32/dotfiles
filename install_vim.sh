2fbcd7560b532318d1de89c5906cf52536c765ad
David Massey <ddmassey@gmail.com>
2024-06-15 16:45:18 -0700

    other stuff

--- install_vim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update && sudo apt-get install curl vim git -y
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
