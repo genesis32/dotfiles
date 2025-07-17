a431b2a47b5f8868ebaff9db97bcb323cda9a45b
D Massey <ddmassey@gmail.com>
2025-07-16 19:41:34 -0700

    fmwrk13

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -99,31 +99,15 @@ require("lazy").setup({
       config = true
   },
   { 'hrsh7th/nvim-cmp', dependencies = {
-          -- Snippet Engine
-          'L3MON4D3/LuaSnip',
-
           -- Completion Sources
           'hrsh7th/cmp-nvim-lsp',
           'hrsh7th/cmp-buffer',
           'hrsh7th/cmp-path',
-          'saadparwaiz1/cmp_luasnip',
-
-          -- Go-specific sources
-          'rafamadriz/friendly-snippets'
       },
       config = function()
           local cmp = require('cmp')
-          local luasnip = require('luasnip')
-
-          -- Load friendly-snippets
-          require('luasnip.loaders.from_vscode').lazy_load()
 
           cmp.setup({
-              snippet = {
-                  expand = function(args)
-                      luasnip.lsp_expand(args.body)
-                  end,
-              }, 
               enabled = function()
                 local buftype = vim.api.nvim_buf_get_option(0, 'buftype')
                 local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
@@ -134,15 +118,11 @@ require("lazy").setup({
                     documentation = cmp.config.window.bordered(),
               },
               mapping = cmp.mapping.preset.insert({
---                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
---                ['<C-f>'] = cmp.mapping.scroll_docs(4),
---                ['<C-Space>'] = cmp.mapping.complete(),
                   ['<C-e>'] = cmp.mapping.abort(),
                   ['<CR>'] = cmp.mapping.confirm({ select = true }),
               }),
               sources = cmp.config.sources({
                   { name = 'nvim_lsp' },
-                  { name = 'luasnip' },
                   { name = 'buffer' },
                   { name = 'path' }
               })
