b6123102b9d82998fd3fcf57a5716958036d6c80
ddm <ddmassey@gmail.com>
2026-06-18 22:31:05 -0700

    install steps

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.ideavimrc $HOME/.ideavimrc
 ln -s $HOME/dotfiles/.config/nvim/init.lua $HOME/.config/nvim/init.lua
 ln -s $HOME/dotfiles/.config/ghostty/config $HOME/.config/ghostty/config
+ln -s $HOME/dotfiles/.vimrc $HOME/.zshrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
